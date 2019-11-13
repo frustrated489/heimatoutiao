df4b881405d6029a41a67d419732d9ad670d24dd
frustrated489 <489394204@qq.com>
2019-11-13 09:23:23 +0800

    黑马头条PC-初始化项目-页面分析-新建登录和主页

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/home/index.vue' 完整路径
+import Home from '../views/home' // 简写路径
+import Login from '../views/login'
 
 Vue.use(VueRouter)
 
 const routes = [{
   path: '/',
+  redirect: '/login'
+}, {
+  path: '/home',
   name: 'home',
   component: Home
+}, {
+  path: '/login',
+  component: Login
 }
   // {
   //   path: '/about',
