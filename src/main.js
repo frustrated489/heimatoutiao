242d2c36ecb7285ea1747e473d2c81970106cd4c
frustrated489 <489394204@qq.com>
2019-11-13 12:01:00 +0800

    黑马头条PC-登录模块-安装axios-配置全局使用

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './styles/index.less'
+import axios from 'axios'
+
+axios.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+Vue.proyotype.$http = axios
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,34 @@
 <template>
-  <div>
-      登录
+  <div class="login">
+    <el-card class="login-card">
+      <div class="title">
+        <img src="../../assets/img/logo_index.png" alt />
+      </div>
+    </el-card>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.login {
+  background-image: url("../../assets/img/login_bg.jpg");
+  background-size: cover;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-card {
+    width: 440px;
+    height: 360px;
+    .title{
+      text-align: center;
+      img{
+        height: 45px;
+      }
+    }
+  }
+}
 </style>
