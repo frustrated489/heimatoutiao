4eb437c9ef71a608453acc859dff6378463adb10
frustrated489 <489394204@qq.com>
2019-11-15 20:28:00 +0800

    控制页面访问权限

--- src/main.js
+++ src/main.js
@@ -5,7 +5,13 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './styles/index.less'
 import axios from 'axios'
-axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 设置axios的常态地址
+// 加载 nprogress 中的指定的样式文件
+// 注意：加载第三方包中的具体文件不需要写具体路径，直接写包名即可
+// 总结就是：'包名/具体文件路径'
+import 'nprogress/nprogress.css'
+
+// 设置axios的常态地址
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 Vue.prototype.$axios = axios // 将axios共享给所有实例使用
 Vue.use(ElementUI) // 注册整个的所有的elementUI 组件 Vue.ues 调用了elementUI里面的一个方法 install=> 调用时传入了Vue 对象
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 // import Home from '../views/home/index.vue' 完整路径
-import Layout from '@/views/layout' // 简写路径
-import Login from '@/views/login'
+import Layout from '../views/layout' // 简写路径
+import Login from '../views/login'
+// @是VueCLI 中提供的一种特殊的路径规则，他直接指向src目录路径
+// 注意：在VueCLI 创建的项目中，无论你在哪里使用@符号，他永远指向src
 import Article from '@/views/article'
 import Publish from '@/views/publish'
 import Home from '@/views/home'
+// 加载nprogress
+import NProgress from 'nprogress'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -46,5 +51,38 @@ const routes = [
 const router = new VueRouter({
   routes
 })
+// 路由的拦截器 beforeEach 方法，该方法接受一个函数作为参数
+// 参数1 ：to，表示去哪里的路由信息
+// 参数2 ： from，表示来自哪里的路由信息
+// 参数3 ： next，他是一个方法，用于路由放行
+// 我们具体要做的就是：判断用户的登录状态，有就通过，没有就跳转到登录页
+router.beforeEach((to, from, next) => {
+  // 开启顶部导航进度条特效
+  NProgress.start()
+  // 如果是非登录页面，我们才校验登录状态
 
+  // 如果访问的是登录页面，则直接放行
+  if (to.path === '/login') {
+    next()
+    // 停止代码往后执行
+    return
+  }
+
+  // 非登录页面，校验登录状态
+
+  // 获取用户token
+  const token = window.localStorage.getItem('user-token')
+  // 判断是否有token， 有就通过
+  if (token) {
+    next()
+  } else {
+    // 没有，就跳转到登录页
+    next('/login')
+  }
+})
+
+router.afterEach((to, from) => {
+  // 结束顶部的导航进度条
+  NProgress.done()
+})
 export default router
